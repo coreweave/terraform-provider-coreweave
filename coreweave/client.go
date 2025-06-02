be3f77aca1b1a934370ab3bb117e627448b2ea81
Lucas Stephens <lstephens@coreweave.com>
2025-06-02 15:23:02 -0500

    fix: update retry policy to allow context.DeadlineExceeded errors (#106)
    * fix: update retry policy to allow context.DeadlineExceeded errors
    
    * fix: allow specifying http_timeout as a variable
    
    * chore: use sensible transport defaults provided by go-retryablehttp

--- coreweave/client.go
+++ coreweave/client.go
@@ -2,9 +2,12 @@ package coreweave
 
 import (
 	"context"
+	"crypto/tls"
 	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
+	"regexp"
 	"time"
 
 	"buf.build/gen/go/coreweave/cks/connectrpc/go/coreweave/cks/v1beta1/cksv1beta1connect"
@@ -17,23 +20,106 @@ import (
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
 )
 
-func NewClient(endpoint string, interceptors ...connect.Interceptor) *Client {
-	rc := retryablehttp.NewClient()
-	rc.HTTPClient = &http.Client{
-		Transport: &http.Transport{
-			Proxy:                 http.ProxyFromEnvironment,
-			ResponseHeaderTimeout: 5 * time.Second,
-			ExpectContinueTimeout: 1 * time.Second,
-		},
-		Timeout: 10 * time.Second,
+var (
+	// A regular expression to match the error returned by net/http when the
+	// configured number of redirects is exhausted. This error isn't typed
+	// specifically so we resort to matching on the error string.
+	redirectsErrorRe = regexp.MustCompile(`stopped after \d+ redirects\z`)
+
+	// A regular expression to match the error returned by net/http when the
+	// scheme specified in the URL is invalid. This error isn't typed
+	// specifically so we resort to matching on the error string.
+	schemeErrorRe = regexp.MustCompile(`unsupported protocol scheme`)
+
+	// A regular expression to match the error returned by net/http when a
+	// request header or value is invalid. This error isn't typed
+	// specifically so we resort to matching on the error string.
+	invalidHeaderErrorRe = regexp.MustCompile(`invalid header`)
+
+	// A regular expression to match the error returned by net/http when the
+	// TLS certificate is not trusted. This error isn't typed
+	// specifically so we resort to matching on the error string.
+	notTrustedErrorRe = regexp.MustCompile(`certificate is not trusted`)
+)
+
+func isCertError(err error) bool {
+	_, ok := err.(*tls.CertificateVerificationError)
+	return ok
+}
+
+func baseRetryPolicy(resp *http.Response, err error) (bool, error) {
+	if err != nil {
+		if v, ok := err.(*url.Error); ok {
+			// Don't retry if the error was due to too many redirects.
+			if redirectsErrorRe.MatchString(v.Error()) {
+				return false, v
+			}
+
+			// Don't retry if the error was due to an invalid protocol scheme.
+			if schemeErrorRe.MatchString(v.Error()) {
+				return false, v
+			}
+
+			// Don't retry if the error was due to an invalid header.
+			if invalidHeaderErrorRe.MatchString(v.Error()) {
+				return false, v
+			}
+
+			// Don't retry if the error was due to TLS cert verification failure.
+			if notTrustedErrorRe.MatchString(v.Error()) {
+				return false, v
+			}
+			if isCertError(v.Err) {
+				return false, v
+			}
+		}
+
+		// The error is likely recoverable so retry.
+		return true, nil
 	}
+
+	// 429 Too Many Requests is recoverable. Sometimes the server puts
+	// a Retry-After response header to indicate when the server is
+	// available to start processing request from client.
+	if resp.StatusCode == http.StatusTooManyRequests {
+		return true, nil
+	}
+
+	// Check the response code. We retry on 500-range responses to allow
+	// the server time to recover, as 500's are typically not permanent
+	// errors and may relate to outages on the server side. This will catch
+	// invalid response codes as well, like 0 and 999.
+	if resp.StatusCode == 0 || (resp.StatusCode >= 500 && resp.StatusCode != http.StatusNotImplemented) {
+		return true, fmt.Errorf("unexpected HTTP status %s", resp.Status)
+	}
+
+	return false, nil
+}
+
+func RetryPolicy(ctx context.Context, resp *http.Response, err error) (bool, error) {
+	if ctx.Err() != nil {
+		// do not retry on context.Canceled errors
+		if errors.Is(ctx.Err(), context.Canceled) {
+			return false, ctx.Err()
+		}
+
+		// context.DeadlineExceeded is retried to handle intermittent timeouts
+		return true, ctx.Err()
+	}
+
+	return baseRetryPolicy(resp, err)
+}
+
+func NewClient(endpoint string, timeout time.Duration, interceptors ...connect.Interceptor) *Client {
+	rc := retryablehttp.NewClient()
+	rc.HTTPClient.Timeout = timeout
 	rc.RetryMax = 10
 	rc.RetryWaitMin = 200 * time.Millisecond
 	rc.RetryWaitMax = 5 * time.Second
 	// Jittered exponential back-off (min*2^n) with capping.
 	rc.Backoff = retryablehttp.DefaultBackoff
 	// Treat only idempotent verbs + 502/503/504 + transport errors as retryable.
-	rc.CheckRetry = retryablehttp.DefaultRetryPolicy
+	rc.CheckRetry = RetryPolicy
 
 	c := rc.StandardClient()
 
