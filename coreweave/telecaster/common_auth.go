8e4810d4ca4a27c5089f1d2d097e641da3c9acb6
Eric Miller <emiller@coreweave.com>
2025-11-25 17:09:16 -0600

    wip: telecaster credentials passthrough

---
+++ coreweave/telecaster/common_auth.go
@@ -0,0 +1,61 @@
+package telecaster
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// basicAuthAttribute returns a reusable HTTP Basic authentication schema.
+func basicAuthAttribute() schema.SingleNestedAttribute {
+	return schema.SingleNestedAttribute{
+		MarkdownDescription: "HTTP Basic authentication credentials.",
+		Optional:            true,
+		Attributes: map[string]schema.Attribute{
+			"username": schema.StringAttribute{
+				MarkdownDescription: "Username for HTTP Basic authentication.",
+				Required:            true,
+				Sensitive:           true,
+				WriteOnly:           true,
+			},
+			"password": schema.StringAttribute{
+				MarkdownDescription: "Password for HTTP Basic authentication.",
+				Required:            true,
+				Sensitive:           true,
+				WriteOnly:           true,
+			},
+		},
+	}
+}
+
+// bearerTokenAttribute returns a reusable Bearer token authentication schema.
+func bearerTokenAttribute() schema.SingleNestedAttribute {
+	return schema.SingleNestedAttribute{
+		MarkdownDescription: "Bearer token authentication credentials.",
+		Optional:            true,
+		Attributes: map[string]schema.Attribute{
+			"token": schema.StringAttribute{
+				MarkdownDescription: "Bearer token value.",
+				Required:            true,
+				Sensitive:           true,
+				WriteOnly:           true,
+			},
+		},
+	}
+}
+
+// authHeadersAttribute returns a reusable custom HTTP headers authentication schema.
+func authHeadersAttribute() schema.SingleNestedAttribute {
+	return schema.SingleNestedAttribute{
+		MarkdownDescription: "Custom HTTP headers for authentication.",
+		Optional:            true,
+		Attributes: map[string]schema.Attribute{
+			"headers": schema.MapAttribute{
+				MarkdownDescription: "Map of HTTP header names to values for authentication.",
+				Required:            true,
+				Sensitive:           true,
+				WriteOnly:           true,
+				ElementType:         types.StringType,
+			},
+		},
+	}
+}
--- coreweave/telecaster/internal/model/endpoint.go
+++ coreweave/telecaster/internal/model/endpoint.go
@@ -1,9 +1,7 @@
 package model
 
 import (
-	"context"
 	"fmt"
-	"maps"
 	"strings"
 
 	typesv1beta1 "bsr.core-services.ingress.coreweave.com/gen/go/coreweave/o11y-mgmt/protocolbuffers/go/coreweave/telecaster/types/v1beta1"
@@ -34,7 +32,6 @@ func (r *ForwardingEndpointRefModel) ToMsg() (msg *typesv1beta1.ForwardingEndpoi
 
 type ForwardingEndpointSpecModel struct {
 	DisplayName types.String                       `tfsdk:"display_name"`
-	Kafka       *ForwardingEndpointKafkaModel      `tfsdk:"kafka"`
 	Prometheus  *ForwardingEndpointPrometheusModel `tfsdk:"prometheus"`
 	S3          *ForwardingEndpointS3Model         `tfsdk:"s3"`
 	HTTPS       *ForwardingEndpointHTTPSModel      `tfsdk:"https"`
@@ -43,7 +40,6 @@ type ForwardingEndpointSpecModel struct {
 func (e *ForwardingEndpointSpecModel) Set(msg *typesv1beta1.ForwardingEndpointSpec) (diagnostics diag.Diagnostics) {
 	e.DisplayName = types.StringValue(msg.DisplayName)
 
-	e.Kafka = nil
 	e.Prometheus = nil
 	e.S3 = nil
 	e.HTTPS = nil
@@ -51,10 +47,6 @@ func (e *ForwardingEndpointSpecModel) Set(msg *typesv1beta1.ForwardingEndpointSp
 	switch kind := msg.WhichConfig(); kind {
 	case typesv1beta1.ForwardingEndpointSpec_Config_not_set_case:
 		diagnostics.AddError("no config set for forwarding endpoint", "Config must be set when using forwarding endpoint")
-	case typesv1beta1.ForwardingEndpointSpec_Kafka_case:
-		var kafka ForwardingEndpointKafkaModel
-		diagnostics.Append(kafka.Set(msg.GetKafka())...)
-		e.Kafka = &kafka
 	case typesv1beta1.ForwardingEndpointSpec_Prometheus_case:
 		e.Prometheus = new(ForwardingEndpointPrometheusModel)
 		diagnostics.Append(e.Prometheus.Set(msg.GetPrometheus())...)
@@ -66,58 +58,6 @@ func (e *ForwardingEndpointSpecModel) Set(msg *typesv1beta1.ForwardingEndpointSp
 		diagnostics.Append(e.HTTPS.Set(msg.GetHttps())...)
 	default:
 		diagnostics.AddError("Unsupported forwarding endpoint type", fmt.Sprintf("unsupported forwarding endpoint config type: %s (%d)", kind.String(), kind))
-	}
-
-	return
-}
-
-type ForwardingEndpointKafkaModel struct {
-	BootstrapEndpoints types.String         `tfsdk:"bootstrap_endpoints"`
-	Topic              types.String         `tfsdk:"topic"`
-	TLS                *TLSConfigModel      `tfsdk:"tls"`
-	ScramAuth          *KafkaScramAuthModel `tfsdk:"scram_auth"`
-}
-
-func (k *ForwardingEndpointKafkaModel) Set(msg *typesv1beta1.KafkaConfig) (diagnostics diag.Diagnostics) {
-	k.BootstrapEndpoints = types.StringValue(msg.BootstrapEndpoints)
-	k.Topic = types.StringValue(msg.Topic)
-
-	var tls *TLSConfigModel
-	if msg.HasTls() {
-		tls = new(TLSConfigModel)
-		tls.Set(msg.Tls)
-	}
-	k.TLS = tls
-
-	switch kind := msg.WhichAuth(); kind {
-	case typesv1beta1.KafkaConfig_Auth_not_set_case:
-		diagnostics.AddError("no auth set for kafka", "Auth must be set when using kafka endpoint")
-	case typesv1beta1.KafkaConfig_Scram_case:
-		scram := new(KafkaScramAuthModel)
-		diagnostics.Append(scram.Set(msg.GetScram())...)
-		k.ScramAuth = scram
-	default:
-		diagnostics.AddError("Unsupported Kafka auth type", fmt.Sprintf("unsupported kafka auth type: %s (%d)", kind.String(), kind))
-	}
-
-	return
-}
-
-func (k *ForwardingEndpointKafkaModel) ToMsg() (msg *typesv1beta1.KafkaConfig, diagnostics diag.Diagnostics) {
-	if k == nil {
-		return nil, nil
-	}
-
-	msg = &typesv1beta1.KafkaConfig{
-		BootstrapEndpoints: k.BootstrapEndpoints.ValueString(),
-		Topic:              k.Topic.ValueString(),
-		Tls:                k.TLS.ToMsg(),
-	}
-
-	if k.ScramAuth != nil {
-		scramAuth, diags := k.ScramAuth.ToMsg()
-		diagnostics.Append(diags...)
-		msg.SetScram(scramAuth)
 	}
 
 	return
@@ -211,203 +151,6 @@ func (h *ForwardingEndpointHTTPSModel) ToMsg() (msg *typesv1beta1.HTTPSConfig, d
 	return
 }
 
-type KafkaScramAuthModel struct {
-	Mechanism types.String `tfsdk:"mechanism"`
-}
-
-func (k *KafkaScramAuthModel) Set(msg *typesv1beta1.KafkaScramAuth) (diagnostics diag.Diagnostics) {
-	if msg.Mechanism == "" {
-		k.Mechanism = types.StringNull()
-	} else {
-		k.Mechanism = types.StringValue(msg.Mechanism)
-	}
-	return
-}
-
-func (k *KafkaScramAuthModel) ToMsg() (msg *typesv1beta1.KafkaScramAuth, diagnostics diag.Diagnostics) {
-	if k == nil {
-		return
-	}
-
-	msg = &typesv1beta1.KafkaScramAuth{
-		Mechanism: k.Mechanism.ValueString(),
-	}
-
-	return
-}
-
-type PrometheusCredentialsModel struct {
-	BasicAuth   *BasicAuthCredentialsModel   `tfsdk:"basic_auth"`
-	BearerToken *BearerTokenCredentialsModel `tfsdk:"bearer_token"`
-	AuthHeaders *AuthHeadersCredentialsModel `tfsdk:"auth_headers"`
-}
-
-func (p *PrometheusCredentialsModel) Set(ctx context.Context, msg *typesv1beta1.PrometheusCredentials) (diagnostics diag.Diagnostics) {
-	if msg == nil {
-		return nil
-	}
-
-	p.BasicAuth = nil
-	p.BearerToken = nil
-	p.AuthHeaders = nil
-
-	switch kind := msg.WhichAuth(); kind {
-	case typesv1beta1.PrometheusCredentials_Auth_not_set_case:
-		diagnostics.AddError("no auth set for prometheus", "Auth must be set when using prometheus endpoint")
-	case typesv1beta1.PrometheusCredentials_BasicAuth_case:
-		p.BasicAuth = new(BasicAuthCredentialsModel)
-		diagnostics.Append(p.BasicAuth.Set(ctx, msg.GetBasicAuth())...)
-	case typesv1beta1.PrometheusCredentials_BearerToken_case:
-		p.BearerToken = new(BearerTokenCredentialsModel)
-		diagnostics.Append(p.BearerToken.Set(ctx, msg.GetBearerToken())...)
-	case typesv1beta1.PrometheusCredentials_AuthHeaders_case:
-		p.AuthHeaders = new(AuthHeadersCredentialsModel)
-		diagnostics.Append(p.AuthHeaders.Set(ctx, msg.GetAuthHeaders())...)
-	default:
-		diagnostics.AddError("Unsupported Prometheus auth type", fmt.Sprintf("unsupported prometheus auth type: %s (%d)", kind.String(), kind))
-	}
-
-	return
-}
-
-func (p *PrometheusCredentialsModel) ToMsg() (msg *typesv1beta1.PrometheusCredentials, diagnostics diag.Diagnostics) {
-	if p == nil {
-		return nil, nil
-	}
-
-	msg = &typesv1beta1.PrometheusCredentials{}
-	implementations := make([]string, 0)
-
-	if p.BasicAuth != nil {
-		implementations = append(implementations, "basic_auth")
-		basicAuth, diags := p.BasicAuth.ToMsg()
-		diagnostics.Append(diags...)
-		msg.SetBasicAuth(basicAuth)
-	}
-	if p.BearerToken != nil {
-		implementations = append(implementations, "bearer_token")
-		bearerToken, diags := p.BearerToken.ToMsg()
-		diagnostics.Append(diags...)
-		msg.SetBearerToken(bearerToken)
-	}
-	if p.AuthHeaders != nil {
-		implementations = append(implementations, "auth_headers")
-		headersModel, diags := p.AuthHeaders.ToMsg()
-		diagnostics.Append(diags...)
-		msg.SetAuthHeaders(headersModel)
-	}
-
-	if len(implementations) != 1 {
-		diagnostics.AddError(
-			"Invalid PrometheusCredentials",
-			fmt.Sprintf("Exactly one of basic_auth, bearer_token, or auth_headers must be set, got %d: %s", len(implementations), strings.Join(implementations, ", ")),
-		)
-	}
-	if diagnostics.HasError() {
-		return nil, diagnostics
-	}
-
-	return
-}
-
-type BasicAuthCredentialsModel struct {
-	Username types.String `tfsdk:"username"`
-	Password types.String `tfsdk:"password"`
-}
-
-func (b *BasicAuthCredentialsModel) Set(ctx context.Context, basicAuth *typesv1beta1.BasicAuthCredentials) (diagnostics diag.Diagnostics) {
-	b.Username = types.StringValue(basicAuth.Username)
-	b.Password = types.StringValue(basicAuth.Password)
-	return
-}
-
-func (b *BasicAuthCredentialsModel) ToMsg() (msg *typesv1beta1.BasicAuthCredentials, diagnostics diag.Diagnostics) {
-	if b == nil {
-		return
-	}
-
-	msg = &typesv1beta1.BasicAuthCredentials{
-		Username: b.Username.ValueString(),
-		Password: b.Password.ValueString(),
-	}
-
-	return
-}
-
-type BearerTokenCredentialsModel struct {
-	Token types.String `tfsdk:"token"`
-}
-
-func (b *BearerTokenCredentialsModel) Set(ctx context.Context, bearerToken *typesv1beta1.BearerTokenCredentials) (diagnostics diag.Diagnostics) {
-	b.Token = types.StringValue(bearerToken.Token)
-	return
-}
-
-func (b *BearerTokenCredentialsModel) ToMsg() (msg *typesv1beta1.BearerTokenCredentials, diagnostics diag.Diagnostics) {
-	if b == nil {
-		return
-	}
-
-	msg = &typesv1beta1.BearerTokenCredentials{
-		Token: b.Token.ValueString(),
-	}
-
-	return
-}
-
-type AuthHeadersCredentialsModel struct {
-	Headers map[string]string `tfsdk:"headers"`
-}
-
-func (h *AuthHeadersCredentialsModel) Set(ctx context.Context, msg *typesv1beta1.AuthHeadersCredentials) (diagnostics diag.Diagnostics) {
-	h.Headers = maps.Clone(msg.Headers)
-	return
-}
-
-func (h *AuthHeadersCredentialsModel) ToMsg() (msg *typesv1beta1.AuthHeadersCredentials, diagnostics diag.Diagnostics) {
-	if h == nil {
-		return
-	}
-
-	msg = &typesv1beta1.AuthHeadersCredentials{
-		Headers: maps.Clone(h.Headers),
-	}
-
-	return
-}
-
-type S3CredentialsModel struct {
-	AccessKeyID     types.String `tfsdk:"access_key_id"`
-	SecretAccessKey types.String `tfsdk:"secret_access_key"`
-	SessionToken    types.String `tfsdk:"session_token"`
-}
-
-func (s *S3CredentialsModel) Set(ctx context.Context, msg *typesv1beta1.S3Credentials) (diagnostics diag.Diagnostics) {
-	s.AccessKeyID = types.StringValue(msg.AccessKeyId)
-	s.SecretAccessKey = types.StringValue(msg.SecretAccessKey)
-	if msg.SessionToken == "" {
-		s.SessionToken = types.StringNull()
-	} else {
-		s.SessionToken = types.StringValue(msg.SessionToken)
-	}
-
-	return
-}
-
-func (s *S3CredentialsModel) ToMsg() (msg *typesv1beta1.S3Credentials, diagnostics diag.Diagnostics) {
-	if s == nil {
-		return
-	}
-
-	msg = &typesv1beta1.S3Credentials{
-		AccessKeyId:     s.AccessKeyID.ValueString(),
-		SecretAccessKey: s.SecretAccessKey.ValueString(),
-		SessionToken:    s.SessionToken.ValueString(),
-	}
-
-	return
-}
-
 func (s *ForwardingEndpointSpecModel) ToMsg() (spec *typesv1beta1.ForwardingEndpointSpec, diagnostics diag.Diagnostics) {
 	if s == nil {
 		return nil, nil
@@ -418,12 +161,6 @@ func (s *ForwardingEndpointSpecModel) ToMsg() (spec *typesv1beta1.ForwardingEndp
 	}
 
 	configuredImplementations := make([]string, 0)
-	if s.Kafka != nil {
-		configuredImplementations = append(configuredImplementations, "kafka")
-		kafkaMsg, diags := s.Kafka.ToMsg()
-		diagnostics.Append(diags...)
-		spec.SetKafka(kafkaMsg)
-	}
 
 	if s.Prometheus != nil {
 		configuredImplementations = append(configuredImplementations, "prometheus")
@@ -458,6 +195,14 @@ func (s *ForwardingEndpointSpecModel) ToMsg() (spec *typesv1beta1.ForwardingEndp
 	}
 
 	return spec, nil
+}
+
+// ForwardingEndpointCredentialsModel holds authentication credentials for forwarding endpoints.
+// Only one of Prometheus, HTTPS, or S3 should be set, matching the endpoint type.
+type ForwardingEndpointCredentialsModel struct {
+	Prometheus *PrometheusCredentialsModel `tfsdk:"prometheus"`
+	HTTPS      *HTTPSCredentialsModel      `tfsdk:"https"`
+	S3         *S3CredentialsModel         `tfsdk:"s3"`
 }
 
 type ForwardingEndpointStatusModel struct {
