d8f5b25bea339c05bbcdfe6ccb67602116d58137
Eric Miller <emiller@coreweave.com>
2026-08-20 20:10:44 -0500

    fix(object_storage): confirm bucket before removing state

--- coreweave/object_storage/resource_bucket_unit_test.go
+++ coreweave/object_storage/resource_bucket_unit_test.go
@@ -3,13 +3,23 @@ package objectstorage
 import (
 	"errors"
 	"fmt"
+	standardhttp "net/http"
 	"testing"
 
 	"github.com/aws/smithy-go"
+	smithyhttp "github.com/aws/smithy-go/transport/http"
 )
 
-func TestIsNoSuchTagSetError(t *testing.T) {
+func TestIsMissingBucketTagSetError(t *testing.T) {
 	t.Parallel()
+
+	httpError := func(statusCode int) error {
+		t.Helper()
+		return &smithyhttp.ResponseError{
+			Response: &smithyhttp.Response{Response: &standardhttp.Response{StatusCode: statusCode}},
+			Err:      errors.New("request failed"),
+		}
+	}
 
 	tests := map[string]struct {
 		err  error
@@ -23,8 +33,19 @@ func TestIsNoSuchTagSetError(t *testing.T) {
 			err:  fmt.Errorf("get bucket tagging: %w", &smithy.GenericAPIError{Code: errNoSuchTagSet}),
 			want: true,
 		},
+		"HTTP 404": {
+			err:  httpError(standardhttp.StatusNotFound),
+			want: true,
+		},
+		"wrapped HTTP 404": {
+			err:  fmt.Errorf("get bucket tagging: %w", httpError(standardhttp.StatusNotFound)),
+			want: true,
+		},
 		"different API error": {
 			err: &smithy.GenericAPIError{Code: ErrNoSuchBucket},
+		},
+		"HTTP 500": {
+			err: httpError(standardhttp.StatusInternalServerError),
 		},
 		"non-API error": {
 			err: errors.New("request failed"),
@@ -35,8 +56,8 @@ func TestIsNoSuchTagSetError(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
-			if got := isNoSuchTagSetError(tt.err); got != tt.want {
-				t.Errorf("isNoSuchTagSetError() = %t, want %t", got, tt.want)
+			if got := isMissingBucketTagSetError(tt.err); got != tt.want {
+				t.Errorf("isMissingBucketTagSetError() = %t, want %t", got, tt.want)
 			}
 		})
 	}
