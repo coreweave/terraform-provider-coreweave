1f295ae947e4448a095c63210f524474ec96ac43
Hamidreza Ghafghazi <hghafghazi@coreweave.com>
2026-08-28 04:33:33 -0700

    feat(workload_federation): add OIDC config resource (#417)

---
+++ examples/resources/coreweave_workload_federation_oidc_config/resource.tf
@@ -0,0 +1,6 @@
+resource "coreweave_workload_federation_oidc_config" "example" {
+  name        = "hcp-terraform"
+  description = "HCP Terraform workload identity"
+  issuer_url  = "https://app.terraform.io"
+  audience    = "https://coreweave.com/iam"
+}
